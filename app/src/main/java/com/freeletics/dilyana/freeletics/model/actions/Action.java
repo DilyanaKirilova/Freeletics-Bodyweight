0845f9016fa0823a1b41b2dcb677611935186c7e
DilyanaKirilova <dilyana.kirilowa@gmail.com>
2017-04-16 06:11:08 +0300

    Video urls and actions changed

--- app/src/main/java/com/freeletics/dilyana/freeletics/model/actions/Action.java
+++ app/src/main/java/com/freeletics/dilyana/freeletics/model/actions/Action.java
@@ -1,22 +1,8 @@
 package com.freeletics.dilyana.freeletics.model.actions;
 
 /**
- * Created by Dilyana on 3/31/2017.
+ * Created by venesa on 16.4.2017 г..
  */
 
-public abstract class Action {
-
-    public enum Category{WORKOUT, EXERCISES}
-
-    private String equipment;
-    protected Category category;
-    protected double points;
-
-    public Action(String equipment) {
-            this.equipment = equipment;
-    }
-
-    public String getEquipment() {
-        return equipment;
-    }
+public interface Action {
 }
